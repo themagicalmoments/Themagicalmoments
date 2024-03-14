6c32efcd1081bd8f1d853a4a4f51a94ea5394511
themagicalmoments <157443107+themagicalmoments@users.noreply.github.com>
2024-03-14 18:14:40 -0500

    simplyCountdown.js

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -116,9 +116,9 @@
     simplyCountdown = function (elt, args) {
         var parameters = extend({
                 year: 2024,
-                month: 3,
+                month: 03,
                 day: 16,
-                hours: 0,
+                hours: 10,
                 minutes: 0,
                 seconds: 0,
                 words: {
